1fb4afcec1083846df583344d557a5b5d174ee12
smerr <smerr13@gmail.com>
2020-07-21 18:45:55 -0700

    fixed server.js and added json package

--- server.js
+++ server.js
@@ -24,13 +24,10 @@ app.use(express.static("website"));
 const port = 8000;
 
 // Spin up the server
-const server = app.listen(port, () => {
-  console.log("port: ${port}");
-});
+const server = app.listen(port, listening);
 
 // Callback to debug
 function listening() {
-  console.log(server);
   console.log(`running on localhost: ${port}`);
 }
 
